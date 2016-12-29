a377c3281798411fc0ca85d212acf5029a30cd84
machenmusik <machenmusik@gmail.com>
2016-12-29 00:18:14 -0500

    changes per discussion on aframe-registry PR

--- dist/aframe-stats-in-vr-component.js
+++ dist/aframe-stats-in-vr-component.js
@@ -62,7 +62,8 @@
 	  schema: {
 	    enabled: {type:'boolean', default: true},
 	    position: {type:'string', default: '0 -0.35 -0.5'},
-	    scale: {type:'string', default: '0.5 0.5 1'}
+	    scale: {type:'string', default: '0.5 0.5 1'},
+	    updateIntervalMillis: {type:'number', default: 500}
 	  },
 
 	  init: function () {
@@ -74,60 +75,66 @@
 	    statsEl.className = 'a-hidden';
 
 	    // defer most initialization until after camera is injected
+	    this.createStatsPanel = this.createStatsPanel.bind(this);
+	    setTimeout(this.createStatsPanel, 0);
+	  },
+
+	  createStatsPanel: function () {
 	    var self = this;
-	    setTimeout(function () {
-	      // attached to scene element, so inject stats panel into camera
-	      self.statspanel = document.createElement('a-entity');
-	      self.statspanel.setAttribute('id', 'statspanel');
-	      self.statspanel.setAttribute('position', self.data.position);
-	      self.statspanel.setAttribute('scale', self.data.scale);
-	      self.statspanel.setAttribute('visible', self.data.enabled ? 'true' : 'false');
-	      self.el.camera.el.appendChild(self.statspanel);
+	    var scene = this.el;
+	    var statsEl = scene.components['stats'].statsEl;
 
-	      // set up the VR stats panel
-	      self.valuecanvases = [];
-	      self.rsids = [];
-	      self.rsvalues = [];
-	      var rscanvases = document.querySelectorAll('.rs-canvas');
-	      for (var i = 0; i < rscanvases.length; i++) {
-	        // remember labels and vale elements
-	        self.rsids.push(rscanvases[i].parentElement.querySelector('.rs-counter-id').innerText);
-	        self.rsvalues.push(rscanvases[i].parentElement.querySelector('.rs-counter-value'));
+	    // attached to scene element, so inject stats panel into camera
+	    self.statspanel = document.createElement('a-entity');
+	    self.statspanel.setAttribute('id', 'statspanel');
+	    self.statspanel.setAttribute('position', self.data.position);
+	    self.statspanel.setAttribute('scale', self.data.scale);
+	    self.statspanel.setAttribute('visible', self.data.enabled ? 'true' : 'false');
+	    self.el.camera.el.appendChild(self.statspanel);
 
-	        // inject id values for rstats canvases
-	        var idsuffix = self.rsids[i].replace(' ', '_');
-	        rscanvases[i].id = 'rstats-' + idsuffix;
+	    // set up the VR stats panel
+	    self.valuecanvases = [];
+	    self.rsids = [];
+	    self.rsvalues = [];
+	    var rscanvases = document.querySelectorAll('.rs-canvas');
+	    for (var i = 0; i < rscanvases.length; i++) {
+	      // remember labels and vale elements
+	      self.rsids.push(rscanvases[i].parentElement.querySelector('.rs-counter-id').innerText);
+	      self.rsvalues.push(rscanvases[i].parentElement.querySelector('.rs-counter-value'));
 
-	        var y = (1.25 - i * 0.025) + ' 0';
+	      // inject id values for rstats canvases
+	      var idsuffix = self.rsids[i].replace(' ', '_');
+	      rscanvases[i].id = 'rstats-' + idsuffix;
 
-	        // create the image for the rstats canvas
-	        var stats = document.createElement('a-image');
-	        stats.setAttribute('position', '-0.08 ' + y);
-	        stats.setAttribute('width', '0.34');
-	        stats.setAttribute('height', '0.025');
-	        stats.setAttribute('src', '#' + rscanvases[i].id);
-	        self.statspanel.appendChild(stats);
+	      var y = (1.25 - i * 0.025) + ' 0';
 
-	        // create the canvas for the value
-	        var valuecanvas = document.createElement('canvas');
-	        valuecanvas.setAttribute('id', 'value-' + idsuffix);
-	        valuecanvas.setAttribute('width', '160');
-	        valuecanvas.setAttribute('height', '20');
-	        valuecanvas.setAttribute('crossorigin', 'anonymous');
-	        self.valuecanvases.push(valuecanvas);
+	      // create the image for the rstats canvas
+	      var stats = document.createElement('a-image');
+	      stats.setAttribute('position', '-0.08 ' + y);
+	      stats.setAttribute('width', '0.34');
+	      stats.setAttribute('height', '0.025');
+	      stats.setAttribute('src', '#' + rscanvases[i].id);
+	      self.statspanel.appendChild(stats);
 
-	        // add the value canvas
-	        statsEl.appendChild(self.valuecanvases[i]);
+	      // create the canvas for the value
+	      var valuecanvas = document.createElement('canvas');
+	      valuecanvas.setAttribute('id', 'value-' + idsuffix);
+	      valuecanvas.setAttribute('width', '160');
+	      valuecanvas.setAttribute('height', '20');
+	      valuecanvas.setAttribute('crossorigin', 'anonymous');
+	      self.valuecanvases.push(valuecanvas);
 
-	        // create the image for the value canvas
-	        var value = document.createElement('a-image');
-	        value.setAttribute('position', '0.17 ' + y);
-	        value.setAttribute('width', '0.16');
-	        value.setAttribute('height', '0.025');
-	        value.setAttribute('src', '#' + self.valuecanvases[i].id);
-	        self.statspanel.appendChild(value);
-	      }
-	    }, 0);
+	      // add the value canvas
+	      statsEl.appendChild(self.valuecanvases[i]);
+
+	      // create the image for the value canvas
+	      var value = document.createElement('a-image');
+	      value.setAttribute('position', '0.17 ' + y);
+	      value.setAttribute('width', '0.16');
+	      value.setAttribute('height', '0.025');
+	      value.setAttribute('src', '#' + self.valuecanvases[i].id);
+	      self.statspanel.appendChild(value);
+	    }
 	  },
 
 	  update: function () {
@@ -145,19 +152,9 @@
 	  },
 
 	  tick: function () {
-	    var scene = this.el;
-	    var stats = scene.components['stats'].stats;
-
-	    if (!stats) { return; }
-
-	    stats('rAF').tick();
-	    stats('FPS').frame();
-	    stats().update();
-
 	    // periodically update the value canvases
 	    var now = Date.now();
-	    if (now < this.lastTime + 500) { return; }
-
+	    if (now < this.lastTime + this.data.updateIntervalMillis) { return; }
 	    this.lastTime = now;
 	    if (this.valuecanvases) {
 	      for (var i = 0; i < this.valuecanvases.length; i++) {
