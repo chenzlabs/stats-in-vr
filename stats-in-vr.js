aa04a4c5a5bca5883578742a54a1d0c822742ffa
Kyle Baker <github@kylebaker.io>
2021-08-19 06:26:55 +0200

    Update stats-in-vr.js
    - Performance improvements
    - adding back the bars!
    - more comfortable default position, size, rotation
    - only show VR component when in VR
    - optionally show standard stats when not in VR (default:true)
    - a touch of opacity makes panel a little less painfully drab
    - add ability to optionally anchor the component in-world instead of to-headset
    - problem: adds a ton of draw calls with current inherited design, by adding a ton of images/canvases. will fix that next.

--- stats-in-vr.js
+++ stats-in-vr.js
@@ -8,93 +8,171 @@ AFRAME.registerComponent("stats-in-vr", {
 
   schema: {
     enabled: { type: "boolean", default: true },
-    position: { type: "string", default: "0 -0.35 -0.5" },
-    scale: { type: "string", default: "0.5 0.5 1" },
-    updateIntervalMillis: { type: "number", default: 500 }
+    position: { type: "string", default: "0 -1.1 -1" },
+    rotation: { type: "string", default: "-20 0 0" },
+    scale: { type: "string", default: "1 .8 1" },
+    updateIntervalMs: { type: "number", default: 250 },
+    show2dstats: { type: "boolean", default: true },
+    anchorEl: { type: "string", default: "[camera]" },
   },
-
+  
+  inVR: false,
   init: function() {
-    var scene = this.el;
-    var statsEl = scene.components["stats"].statsEl;
+    
+    // var scene = this.sceneEl;
+    // var statsEl = this.sceneEl.components.stats.statsEl;
+    // this.tick = AFRAME.utils.throttleTick(this.tick, this.data.updateIntervalMs, this);
+    this.canvasParent = document.createElement('div');
+    this.canvasParent.setAttribute('id','stats-in-vr-canvas-parent')
+    this.sceneEl = AFRAME.scenes[0]
+    
+    AFRAME.scenes[0].addEventListener('enter-vr', async () => {
+      this.inVR = true;
+      if (this.data.enabled) {
+        this.show()
+      }
+      // AFRAME.scenes[0].setAttribute('stats-in-vr',{a:1})
+    })
+    AFRAME.scenes[0].addEventListener('exit-vr', async () => {
+      this.inVR = false;
+      this.hide();
+      // AFRAME.scenes[0].setAttribute('stats-in-vr',{a:1})
+    })
+    
+    if (!this.data.show2dstats) {
+      // hide the DOM stats panel, except for ours
+      this.sceneEl.components.stats.statsEl/*.querySelector('.rs-container')*/.style = 'display: none !important;';
+      this.sceneEl.components.stats.statsEl/*.querySelector('.rs-container')*/.className = 'a-hidden';
+    }
 
-    // hide the DOM stats panel
-    //statsEl.style = 'display: none !important;';
-    //statsEl.className = 'a-hidden';
-
+    this.begin();
+  },
+  
+  begin: async function() {
     // once we start rendering, create VR stats panel
-    if (scene.renderStarted) {
-      this.createStatsPanel();
+    if (this.sceneEl.renderStarted) {
+      await this.createStatsPanel();
     } else {
-      scene.addEventListener("renderstart", this.createStatsPanel.bind(this));
+      this.sceneEl.addEventListener("renderstart", this.createStatsPanel.bind(this));
     }
   },
 
-  createStatsPanel: function() {
-    var self = this;
-
+  createStatsPanel: async function() {
     // attached to scene element, so inject stats panel into camera
-    self.statspanel = document.createElement("a-entity");
-    self.statspanel.setAttribute("id", "statspanel");
-    self.statspanel.setAttribute("position", self.data.position);
-    self.statspanel.setAttribute("scale", self.data.scale);
-    self.statspanel.setAttribute(
+    this.statspanel = document.createElement("a-entity");
+    this.statspanel.setAttribute("id", "statspanel");
+    this.statspanel.setAttribute("position", this.data.position);
+    this.statspanel.setAttribute('rotation', this.data.rotation)
+    this.statspanel.setAttribute("scale", this.data.scale);
+    this.statspanel.setAttribute(
       "visible",
-      self.data.enabled ? "true" : "false"
+      this.data.enabled ? "true" : "false"
     );
-    self.el.camera.el.appendChild(self.statspanel);
-
-    // set up the VR stats panel
-    self.valuecanvases = [];
-    self.rsids = [];
-    self.rsvalues = [];
+    // this.sceneEl.camera.el.appendChild(this.statspanel);
+    document.querySelector(this.data.anchorEl).appendChild(this.statspanel);
+    
+    await this.addEls();
+    if (!this.inVR) {
+      // console.warn("disabled for dev, but add this line back in")
+      this.hide();
+    }
+    this.sceneEl.components.stats.statsEl.appendChild(this.canvasParent);
+    this.addStatPanels();
+    this.tick = AFRAME.utils.throttleTick(this.willtick, this.data.updateIntervalMs, this);
   },
-
-  updateStatsPanel: function() {
-    var self = this;
-    if (!self.statspanel || !self.rsids) {
+  
+  waitForTime: async function(ms) {
+    return new Promise((res,rej) => {
+      setTimeout(res,ms)
+    })
+  },
+  
+  addEls: async function() {
+    if (!document.querySelectorAll(".rs-canvas").length) {
+      // console.log("no canvases, recurse in ", this.data.updateIntervalMs)
+      await this.waitForTime(this.data.updateIntervalMs)
+      await this.addEls()
+    }
+    // set up the VR stats panel
+    this.valuecanvases = [];
+    this.valueimages = [];
+    this.rsids = [];
+    this.rsvalues = [];
+    this.stats = [];
+    // console.log(document.querySelectorAll(".rs-canvas").length)
+    // this.addStatPanels();
+  },
+  
+  addStatPanels: function() {
+    // method:
+    // get labels
+    // get values
+    // removed, to add back: get raw bar canvas
+    // make canvas for every label/value
+    // make picture with prior canvas as source
+    // attach picture to statspanel entity
+    // set canvas in the canvas ctx within the tick function (weird?)
+    // label the item as needing an update to rendered
+    
+    if (!this.statspanel || !this.rsids) {
       return;
     }
-    var scene = this.el;
-    var statsEl = scene.components["stats"].statsEl;
-    var rscanvases = document.querySelectorAll(".rs-canvas");
-    for (var i = 0; i < rscanvases.length; i++) {
-      var rsparent = rscanvases[i].parentElement;
-      var rsid = rsparent.querySelector(".rs-counter-id").innerText;
+    
+    if (!this.rscanvases) {
+      this.rscanvases = document.querySelectorAll(".rs-canvas");
+    }
+    
+    
+    // for (var i = 0; i < this.rscanvases.length; i++) {
+    for (var i = 0; i < this.rscanvases.length; i++) {
+    
+      this.yval = (1.25 - i * 0.025);
+      this.rsparent = this.rscanvases[i].parentElement;
+      this.rsid = this.rsparent.querySelector(".rs-counter-id").innerText;
 
-      if (self.rsids.indexOf(rsid) >= 0) {
+      if (this.rsids.indexOf(this.rsid) >= 0) {
         continue;
       }
 
       // remember labels and value elements
-      self.rsids.push(rsid);
-      self.rsvalues.push(rsparent.querySelector(".rs-counter-value"));
+      // this.rsids.push(this.rsid);
+      this.rsids[i] = this.rsid;
+      this.rsvalues[i] = (this.rsparent.querySelector(".rs-counter-value"));
 
       // inject id values for rstats canvases
-      var idsuffix = self.rsids[i].replace(" ", "_");
-      rscanvases[i].id = "rstats-" + idsuffix;
-
-      var y = 1.25 - i * 0.025 + " 0";
+      this.idsuffix = this.rsids[i].replace(" ", "_");
+      this.rscanvases[i].id = "rstats-" + this.idsuffix;
+      
+      
+      // create the image for the rstats canvas
+      this.stats[i] = document.createElement('a-image');
+      this.stats[i].setAttribute('position', {x:-0.08, y:this.yval, z:0});
+      this.stats[i].setAttribute('width', '0.34');
+      this.stats[i].setAttribute('height', '0.025');
+      this.stats[i].setAttribute('src', '#' + this.rscanvases[i].id);
+      this.statspanel.appendChild(this.stats[i]);      
 
       // create the canvas for the value
-      var valuecanvas = document.createElement("canvas");
-      valuecanvas.setAttribute("id", "value-" + idsuffix);
-      valuecanvas.setAttribute("width", "192");
-      valuecanvas.setAttribute("height", "16");
-      valuecanvas.setAttribute("crossorigin", "anonymous");
-      self.valuecanvases.push(valuecanvas);
+      // var valuecanvas = document.createElement("canvas");
+      this.valuecanvases[i] = this.valuecanvases[i] || document.createElement("canvas");
+      this.valuecanvases[i].setAttribute("id", "value-" + this.idsuffix);
+      this.valuecanvases[i].setAttribute("width", "128");
+      this.valuecanvases[i].setAttribute("height", "16");
+      this.valuecanvases[i].setAttribute("crossorigin", "anonymous");
 
       // add the value canvas
-      statsEl.appendChild(self.valuecanvases[i]);
+      this.canvasParent.appendChild(this.valuecanvases[i])
+      // this.sceneEl.components.stats.statsEl.appendChild(this.canvasParent);
 
       // create the image for the value canvas
-      var value = document.createElement("a-image");
-      value.setAttribute("id", "aframe-" + idsuffix);
-      value.setAttribute("position", "0.17 " + y);
-      value.setAttribute("width", "0.16");
-      value.setAttribute("height", "0.025");
-      value.setAttribute("src", "#" + self.valuecanvases[i].id);
-      self.statspanel.appendChild(value);
-
+      // var value = document.createElement("a-image");
+      this.valueimages[i] = this.valueimages[i] || document.createElement("a-image");
+      this.valueimages[i].setAttribute("id", "aframe-" + this.idsuffix);
+      this.valueimages[i].setAttribute("position", {x:0.17, y:this.yval, z:0});
+      this.valueimages[i].setAttribute("width", .16);
+      this.valueimages[i].setAttribute("height", .025);
+      this.valueimages[i].setAttribute("src", "#" + this.valuecanvases[i].id);
+      this.statspanel.appendChild(this.valueimages[i]);
     }
   },
 
@@ -104,38 +182,39 @@ AFRAME.registerComponent("stats-in-vr", {
     }
     this.statspanel.setAttribute("position", this.data.position);
     this.statspanel.setAttribute("scale", this.data.scale);
-    return !this.data.enabled ? this.hide() : this.show();
+    this.tick = AFRAME.utils.throttleTick(this.willtick, this.data.updateIntervalMs, this);
+    return this.data.enabled ? this.show() : this.hide();
   },
 
   remove: function() {
-    var scene = this.el;
-    var statsEl = scene.components["stats"].statsEl;
+    var statsEl = this.sceneEl.components.stats.statsEl;
 
     statsEl.parentNode.removeChild(statsEl);
   },
 
-  tick: function() {
+  tick(){},
+  willtick: function(log) {
     // periodically update the value canvases
-    var now = Date.now();
-    if (now < this.lastTime + this.data.updateIntervalMillis) {
+    if (!this.inVR) {
+      // console.warn("disabled for dev, but add this line back in")
       return;
     }
-    this.lastTime = now;
+
     if (this.valuecanvases) {
-      this.updateStatsPanel();
+      // this.addStatPanels(); // if you had new stats being added dynamically, you could run this...
       for (var i = 0; i < this.valuecanvases.length; i++) {
         var ctx = this.valuecanvases[i].getContext("2d");
         ctx.clearRect(0, 0, 192, 16);
         ctx.font = "12px monospace";
-        ctx.fillStyle = "gray";
+        ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
         ctx.fillRect(0, 0, 192, 16);
         ctx.fillStyle = "black";
-        ctx.fillText(this.rsvalues[i].innerText + " " + this.rsids[i], 2, 16);
-
+        ctx.fillText(this.rsvalues[i].innerText + " " + this.rsids[i], 2, 13);
+      }
+      for (i = 0; i < this.valuecanvases.length*2; i++) {
         if (this.statspanel.childNodes.item(i).components?.material?.shader){
           this.statspanel.childNodes.item(i).components.material.material.map.needsUpdate = true;
-        }
-
+        } 
       }
     }
   },
@@ -143,6 +222,13 @@ AFRAME.registerComponent("stats-in-vr", {
   hide: function() {
     if (this.statspanel) {
       this.statspanel.object3D.visible = false;
+    }
+    if (!this.data.show2dstats) {
+      this.sceneEl.components.stats.statsEl/*.querySelector('.rs-container')*/.style = 'display: none !important;';
+      this.sceneEl.components.stats.statsEl/*.querySelector('.rs-container')*/.className = 'a-hidden';
+    } else {
+      this.sceneEl.components.stats.statsEl.classList.remove("a-hidden");
+      this.sceneEl.components.stats.statsEl.style = "";
     }
   },
 
